8359822fabedaa90bf717d3d7148d30c755f5bc1
Joey Cadieux <instructor.joeycadieux@gmail.com>
2024-02-20 22:44:36 +0000

    inm.

---
+++ script.js
@@ -0,0 +1,21 @@
+var cssRule =
+  "color: firebrick;" +
+  "font-size: 70px;" +
+  "font-weight: bold;" +
+  "text-shadow: 1px 1px 10px rgb(0, 0, 0);" +
+  "filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);";
+console.log("%cStop!", cssRule);
+
+var cssRule =
+  "color: black;" +
+  "padding: 1rem;" +
+  "font-size: 25px;" +
+  "font-weight: sans-serif;" +
+  "text-shadow: none;" +
+  "border-radius: 15px;" +
+  "background-color: 	#FFFF66;";
+("filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);");
+console.log(
+  "%c Warning! Using this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS.Do not enter or paste code that you do not understand.",
+  cssRule,
+);
